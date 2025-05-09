e425a967f55b534217b36f87dbc7d63f40361a01
Abraao-Morais <duarte.abraao30@gmail.com>
2025-05-09 20:54:22 -0300

    REQ01 - Configurando projeto base

---
+++ collabtaskapi/src/test/java/com/example/collabtaskapi/CollabtaskapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.collabtaskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollabtaskapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
